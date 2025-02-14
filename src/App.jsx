df6e66634bd8a42c1288e7e358a5691d4dfdfcb3
Christopher Ambatti <chrisambatti123@gmail.com>
2025-02-14 18:10:04 +0530

    img commit

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,11 @@ function App() {
 
   return (
     
-    <> <a href="#"><img  src="public/logo.png" className="mx-3 mt-3"></img></a> 
+    <> 
+    {/* <a href="#"><img  src="public/logo.png" className="mx-3 mt-3"></img></a> 
+     */}
+     <a href="#"><img src="/logo.png" className="mx-3 mt-3" alt="Logo" /></a>
+
       <div className="container mt-5 ">
       {!loading && !results && (
         <div className="container form-container ">
